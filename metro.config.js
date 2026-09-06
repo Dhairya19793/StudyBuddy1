cb1975a678147b5ad6925a2c40170c876b815af8
Dhruv235 <118232536+Dhruv235@users.noreply.github.com>
2026-09-05 20:03:17 -0700

    Updated metro.config.js

--- metro.config.js
+++ metro.config.js
@@ -2,7 +2,4 @@ const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
-config.resolver.platformMinimized = true;
-config.resolver.platforms = ['ios', 'android', 'native', ...config.resolver.platforms];
-
 module.exports = config;
